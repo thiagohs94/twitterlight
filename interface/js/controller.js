b1734edbf6fa2c534e5aa5bcf060057f4c2a001f
Thiago <s.thiagoh@gmail.com>
2018-10-07 14:50:14 -0300

    adição de biblioteca de CORS

--- interface/js/controller.js
+++ interface/js/controller.js
@@ -1,11 +1,46 @@
-$(document).ready(function() {
+//timeline
+var mensagens = [];
+
+function carregarMensagens(id){
     $.ajax({
     	type: "GET",
-        url: "http://0.0.0.0:5000/posts?usuario_id=11",
+        url: "http://0.0.0.0:5000/posts?usuario_id=" + id,
     	dataType: "json"
-    }).then(function(data) {
-    	console.log(data); 	
-       	//$('.greeting-id').append(data.id);
-       	//$('.greeting-content').append(data.content);
-    });
-});
+    })
+	.done(function(data) {
+		console.log(data);
+		if(data.status == 1 && data.hasOwnProperty("mensagens")){
+			mensagens = data.mensagens;
+			console.log("mensagens");
+			console.log(mensagens);
+
+			for(i=0;i<mensagens.length;i++){
+				exibirMensagem(mensagens[i]);
+			}
+		}
+		else{
+			return null;
+		}
+	})
+	.fail(function() {
+		console.log(data);
+		return null;
+	})
+}
+
+function exibirMensagem(mensagem){
+	//$("#main-timeline").append("olar");
+	var template = $("#template-mensagem").clone(true).attr("id", "msg-" + mensagem.id);
+	template.find("#msg-usuario").html(mensagem.usuario_id);
+	template.find("#msg-texto").html(mensagem.msg-texto);
+    $("#main-timeline").append(template);
+    $("#msg-" + mensagem.id).show();
+}
+
+$(document).ready(function() {
+	var user_id = localStorage.getItem("user_id");
+
+	if (user_id != null){
+		carregarMensagens(user_id);
+	}
+})
