d91136498154252c0d4a8e68eea21ea87e2eebda
Thiago Santos <s.thiagoh@gmail.com>
2018-10-09 15:49:33 -0300

    commit

--- usuarios/model.py
+++ usuarios/model.py
@@ -26,12 +26,13 @@ class User:
 
 	@staticmethod
 	def buscarPorId(id):
-		query = "SELECT username, senha, nome, bio, id FROM Users WHERE id = " + str(id) + ';'
- 		con = Conexao()
+		query = "SELECT username, senha, nome, bio, id FROM Users WHERE id = " + str(id)
+
+		con = Conexao()
 		result = con.carregar(query)
 
  		if result is not None:
-			return User(result[0][0], result[0][1], result[0][2], result[0][3], result[0][4] )
+			return User(result[0][0], result[0][1], result[0][2], result[0][3], result[0][4])
 		else:
 			return None
 
@@ -41,9 +42,9 @@ class User:
 
 		for i,usuario_id in enumerate(ids,start=0):
 			if(i==0):
-				query += "id=" + str(usuario_id) + " ";
+				query += "id=" + str(usuario_id) + " "
 			else:
-				query += "OR id=" + str(usuario_id) + " ";
+				query += "OR id=" + str(usuario_id) + " "
 
 		con = Conexao()
 		result = con.carregar(query)
